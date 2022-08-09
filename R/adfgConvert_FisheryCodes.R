9a6f08c30eb40225b22f0296b8a0bba35abc86b9
William Stockhausen <william.stockhausen@noaa.gov>
2022-08-09 14:25:36 -0700

    2022.08.09. Revised adfgRead_MPD to ignore "eastwest" column in MPD file (it has NOTHING to do with Tanner crab east/west 166W designation--seems to be more like a dateline sort of thing).

--- R/adfgConvert_FisheryCodes.R
+++ R/adfgConvert_FisheryCodes.R
@@ -3,31 +3,44 @@
 #'
 #' @description Function to parse 4-character ADFG "fishery" codes.
 #'
-#' @param f -  vetor of fishery codes to parse
+#' @param f -  vector of fishery codes to parse
 #'
 #' @return dataframe with columns:
-#' * fishery code - f
+#' * fishery code - \code{f} (input vector of ADFG codes)
 #' * fishery - RKC, snow crab, Tanner crab
 #' * area - Bering Sea, Bristol Bay, CDQ, East, West, UNKNOWN
 #' * code_year - year corresponding to fishery code (NOT! fishery year, apparently)
+#' * char12 - first two characters of fishery code
 #'
-#' @details Uses \code{stringr::sub}, \code{wtsUtilities::substituteValues} functions.
+#' @details Uses \code{stringr::str_sub}, \code{wtsUtilities::substituteValues} functions.
+#'
+#' @importFrom dplyr bind_rows
+#' @importFrom stringr str_sub
+#' @importFrom tibble tibble
+#' @importFrom wtsUtilities substituteValues
 #'
 #' @export
 #'
 adfgConvert_FisheryCodes<-function(f){
   char12 <- stringr::str_sub(f,start=1,end=2);
   char34 <- stringr::str_sub(f,start=3,end=4);
-  dfr.12 <- rbind(data.frame(orig="CK",target="RKC or BKC", area= "CDQ",        stringsAsFactors=FALSE),
-                  data.frame(orig="CO",target="snow crab",  area= "CDQ",        stringsAsFactors=FALSE),
-                  data.frame(orig="CR",target="RKC",        area= "CDQ",        stringsAsFactors=FALSE),
-                  data.frame(orig="EI",target="Tanner crab",area="UNKNOWN",     stringsAsFactors=FALSE),
-                  data.frame(orig="EO",target="snow crab",  area="UNKNOWN",     stringsAsFactors=FALSE),
-                  data.frame(orig="QO",target="snow crab",  area= "Bering Sea", stringsAsFactors=FALSE),
-                  data.frame(orig="QR",target="RKC",        area= "Bering Sea ",stringsAsFactors=FALSE),
-                  data.frame(orig="QT",target="Tanner crab",area= "West",       stringsAsFactors=FALSE),
-                  data.frame(orig="TR",target="RKC",        area= "Bristol Bay",stringsAsFactors=FALSE),
-                  data.frame(orig="TT",target="Tanner crab",area= "East",       stringsAsFactors=FALSE));
+  dfr.12 <- dplyr::bind_rows(
+              tibble::tibble(orig="CK",target="RKC or BKC", area= "CDQ"),
+              tibble::tibble(orig="CO",target="snow crab",  area= "CDQ"),
+              tibble::tibble(orig="CR",target="RKC",        area= "CDQ"),
+              tibble::tibble(orig="EI",target="Tanner crab",area= "UNKNOWN"),
+              tibble::tibble(orig="EO",target="snow crab",  area= "UNKNOWN"),
+              tibble::tibble(orig="QB",target="GKC",        area= "Pribilof Islands"),
+              tibble::tibble(orig="QO",target="snow crab",  area= "Bering Sea"),
+              tibble::tibble(orig="QP",target="BKC",        area= "St. Matthew Island"),
+              tibble::tibble(orig="QR",target="RKC",        area= "Bering Sea "),
+              tibble::tibble(orig="QT",target="Tanner crab",area= "West"),
+              tibble::tibble(orig="SB",target="GKC",        area= "St. Matthew Island"),
+              tibble::tibble(orig="TB",target="GKC",        area= "Bristol Bay"),
+              tibble::tibble(orig="TR",target="RKC",        area= "Bristol Bay"),
+              tibble::tibble(orig="TT",target="Tanner crab",area= "East"),
+              tibble::tibble(orig="XR",target="RKC",        area= "East")
+            );
 
   fishery <- wtsUtilities::substituteValues(char12,
                                             dfr.12$orig,
@@ -37,5 +50,5 @@ adfgConvert_FisheryCodes<-function(f){
                                             dfr.12$area);
   num34 <- as.numeric(char34);
   year <- ifelse(num34>50,1900+num34,2000+num34);
-  return(data.frame(list(fishery_code=f,fishery=fishery,area=area,code_year=year),stringsAsFactors=FALSE));
+  return(tibble::tibble(fishery_code=f,fishery=fishery,area=area,code_year=year,char12=char12));
 }
--- R/adfgRead_MPD.R
+++ R/adfgRead_MPD.R
@@ -5,6 +5,7 @@
 #'
 #' @param csv - csv filename with measure pot data
 #' @param date_format - string ("yyyy-mm-dd" or "mm-dd-yyyy") indicating date format
+#' @param returnDropped - flag (T/F) to return *dropped* records, not selected records
 #'
 #' @return a tibble with columns "fishery","area","EWbySA","EWbyLon","year","fishery_code",
 #' "code_year","trip","adfg","sampdate","spn","statarea","mi_lon","mi_lat",
@@ -19,6 +20,7 @@
 #' @note 'count' is the number of measured crab associated with the rest of the row categories.
 #' Unlike the dockside data, this  is 1 for all rows (i.e., one row for each measured crab).
 #'
+#' @importFrom dplyr anti_join
 #' @importFrom readr read_csv
 #' @importFrom stringr str_sub
 #'
@@ -27,7 +29,8 @@
 #' @export
 #'
 adfgRead_MPD<-function(csv="crab_dump.csv",
-                       date_format="yyyy-mm-dd"){
+                       date_format="yyyy-mm-dd",
+                       returnDropped=FALSE){
   #--read measure pot data file
   dfr <- readr::read_csv(csv,guess_max=1000000);
   #expected column names are:
@@ -55,11 +58,9 @@ adfgRead_MPD<-function(csv="crab_dump.csv",
   }
   latCol = llCols[1];
   lonCol = llCols[2];
-  #----check for eastwest column
-  eastwest = FALSE;
-  if (any(names(dfr)=="eastwest")) eastwest = TRUE;
-  if (!any(names(dfr %in% c(expColNames,llCols)))){
-    idx<-!(names(dfr) %in% c(expColNames,llCols,"eastwest"));
+  #----check for extra column
+  if (any(!(names(dfr) %in% c(expColNames,llCols)))){
+    idx<-!(names(dfr) %in% c(expColNames,llCols));
     str<-paste0("The following unrecognized columns will be skipped \n\t",paste(names(dfr)[idx],collapse=", "));
     warning(str,immediate.=TRUE);#--will continue without extra (unmatched) columns
   }
@@ -67,19 +68,48 @@ adfgRead_MPD<-function(csv="crab_dump.csv",
   nr_orig = nrow(dfr);
   message("\tnumber of input rows: ",nr_orig);
 
-  #unique fishery names:
-  # [1] "CK98" "CO00" "CO01" "CO02" "CO03" "CO04" "CO05" "CO98" "CO99" "CR00" "CR01" "CR02" "CR03" "CR04" "CR98" "CR99" "EI91" "EI92" "EO91" "EO92" "EO93" "QO00" "QO01"
-  #[24] "QO02" "QO03" "QO04" "QO05" "QO06" "QO07" "QO08" "QO09" "QO10" "QO11" "QO12" "QO13" "QO14" "QO15" "QO16" "QO17" "QO94" "QO95" "QO96" "QO97" "QO98" "QO99" "QR93"
-  #[47] "QR95" "QT05" "QT06" "QT07" "QT08" "QT13" "QT14" "QT15" "QT17" "QT93" "QT94" "QT95" "QT96" "TR00" "TR01" "TR02" "TR03" "TR04" "TR05" "TR06" "TR07" "TR08" "TR09"
-  #[70] "TR10" "TR11" "TR12" "TR13" "TR14" "TR15" "TR16" "TR17" "TR90" "TR91" "TR92" "TR93" "TR96" "TR97" "TR98" "TR99" "TT06" "TT07" "TT08" "TT09" "TT13" "TT14" "TT15"
-  #unique crab areas (character 1): C (CDQ fisheries), E (???), Q (Bering Sea), T (Bristol Bay)
+  #unique fishery names (2022):
+#  [1] "CK98"  "CO00"  "CO01"  "CO02"  "CO03"  "CO04"  "CO05"  "CO98"  "CO99"  "CR00"  "CR01"  "CR02"  "CR03"  "CR04"  "CR98"
+#  [16] "CR99"  "EI91"  "EI92"  "EO91"  "EO92"  "EO93"  "QB01"  "QB02"  "QB03"  "QB04"  "QB05"  "QB10"  "QB11"  "QB17"  "QB18"
+#  [31] "QB19"  "QB20"  "QB21"  "QO00"  "QO01"  "QO02"  "QO03"  "QO04"  "QO05o" "QO05r" "QO06"  "QO07"  "QO08"  "QO09"  "QO10"
+#  [46] "QO11"  "QO12"  "QO13"  "QO14"  "QO15"  "QO16"  "QO17"  "QO18"  "QO19"  "QO20"  "QO21"  "QO94"  "QO95"  "QO96"  "QO97"
+#  [61] "QO98"  "QO99"  "QP09"  "QP10"  "QP11"  "QP12"  "QP14"  "QP15"  "QR93"  "QR95"  "QT05"  "QT06"  "QT07"  "QT08"  "QT13"
+#  [76] "QT14"  "QT15"  "QT17"  "QT18"  "QT20"  "QT21"  "QT93"  "QT94"  "QT95"  "QT96"  "SB19"  "TB02"  "TR00"  "TR01"  "TR02"
+#  [91] "TR03"  "TR04"  "TR05"  "TR06"  "TR07"  "TR08"  "TR09"  "TR10"  "TR11"  "TR12"  "TR13"  "TR14"  "TR15"  "TR16"  "TR17"
+# [106] "TR18"  "TR19"  "TR20"  "TR90"  "TR91"  "TR92"  "TR93"  "TR96"  "TR97"  "TR98"  "TR99"  "TT06"  "TT07"  "TT08"  "TT09"
+# [121] "TT13"  "TT14"  "TT15"  "XR06"  "XR07"  "XR09"  "XR13"  "XR14"  "XR15"  "XR16"  "XR17"  "XR18"  "XR19"  "XR20"  "XR21"
+  #unique crab areas (character 1)
+  #  C: CDQ fisheries
+  #  E (???),
+  #  Q: Bering Sea
+  #  T: Bristol Bay
+  #  X: Recovery fishery (principally BBRKC)
 
-  #unique targets (character 2): K (red or blue king crab), O (snow crab), R (red king crab), I (Tanner crab), T (Tanner crab)
+  #unique targets (character 2)
+  #  B: golden (brown) king crab
+  #  K: red or blue king crab
+  #  O: snow crab
+  #  P: blue king crab
+  #  R: red king crab
+  #  I: Tanner crab
+  #  T: Tanner crab
 
-  #unique area/targets: "CK" (CDQ red or blue king crab), "CO" (CDQ snow crab), "CR" (CDQ red ling crab),
-  #                     "EI" (?? Tanner crab), "EO" (??? snow crab),
-  #                     "QO" (Bering Sea snow crab) "QR" (Bering Sea red king crab) "QT" (Tanner crab West),
-  #                     "TR" (BBRKC) "TT", (Tanner crab East)
+  #unique area/targets
+  #  CK: CDQ red or blue king crab
+  #  CO: CDQ snow crab
+  #  CR: CDQ red king crab
+  #  EI: ?? Tanner crab
+  #  EO: ??? snow crab
+  #  QB: Pribilof Islands golden king crab
+  #  QO: Bering Sea snow crab
+  #  QP: Saint Matts BKC
+  #  QR: Bering Sea red king crab
+  #  QT: Tanner crab West
+  #  SB: Saint Matts GKC
+  #  TB: BB GKC must have been an exploratory fishery
+  #  TR: BBRKC
+  #  TT: Tanner crab East
+  #  XR: BBRKC cost recovery
 
   #convert sex codes to labels
   dfr$sex <- adfgConvert_SexCodes(dfr$sex);
@@ -99,25 +129,19 @@ adfgRead_MPD<-function(csv="crab_dump.csv",
   #convert maturity codes
   # maturity:
 
-  #--extract or assign E/W 166W
-  if (eastwest) {
-    dfr$EWbyLon <- ifelse(dfr$eastwest=="E","East 166W","West 166W");
-    dfr$EWbySA  <- ifelse(dfr$eastwest=="E","East 166W","West 166W");
-  } else {
-    #--assign E/W 166W area based on middle longitude of pot string
-    dfr$EWbyLon <- ifelse(-166<dfr[[lonCol]],"East 166W","West 166W");
-    #--assign E/W 166W area based on statarea code (XXYYYY, where XX indicates lon of eastern edge of ADFG stat area)
-    dfr$EWbySA  <- ifelse(as.numeric(stringr::str_sub(as.character(dfr$statarea),1,2))>=66,"West 166W","East 166W");
-  }
+  #--assign E/W 166W area based on middle longitude of pot string
+  dfr$EWbyLon <- ifelse(-166<dfr[[lonCol]],"East 166W","West 166W");
+  #--assign E/W 166W area based on statarea code (XXYYYY, where XX indicates lon of eastern edge of ADFG stat area)
+  dfr$EWbySA  <- ifelse(as.numeric(stringr::str_sub(as.character(dfr$statarea),1,2))>=66,"West 166W","East 166W");
 
   #--parse 4-character fishery codes
   dfr.pf<-adfgConvert_FisheryCodes(dfr$fishery);
 
-  #combine columns, standardize lat/lon column names, and drop "fishery" column
-  dfrp <- cbind(dfr,dfr.pf)
+  #combine columns, standardize lat/lon column names, drop original "fishery" column
+  dfrp <- dplyr::bind_cols(dfr %>% dplyr::select(!fishery),dfr.pf);
   dfrp$mi_lat = dfrp[[latCol]];
   dfrp$mi_lon = dfrp[[lonCol]];
-  dfrp %>% dplyr::select(!tidyselect::any_of(c("fishery","latitude","longitude")));
+  dfrp %<>% dplyr::select(!tidyselect::any_of(c("latitude","longitude")));
   dfrp$count <- 1;#--add 'count': each row represents an observation on 1 crab
   #--determine crab year corresponding to sample date
   if (date_format=="yyyy-mm-dd"){
@@ -156,6 +180,11 @@ adfgRead_MPD<-function(csv="crab_dump.csv",
   dfrp1 <- dfrp[(!idr)&(idk2|idk4),]; #select subset
   message("\tOther rows dropped: ",sum(!(idk2|idk4)))
   message("\tdropped ",sum(!((!idr)&(idk2|idk4)))," total rows at stage 1.")
+
+  if (returnDropped){
+    dfrp2 = dfrp %>% dplyr::anti_join(dfrp1);
+    return(dfrp2);
+  }
 
   #assign area designations "all EBS", "East 166W" and "West 166W"
   dfrp1$area <- "all EBS"; #all RKC and snow crab
